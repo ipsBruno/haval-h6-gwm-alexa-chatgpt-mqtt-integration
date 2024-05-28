c3371f59c900e1920f822facc38adc56d2968878
bobaoapae <jvborges@outlook.com>
2024-05-27 21:08:15 -0300

    remove node_modules from git. correct .gitignore file.

--- node_modules/node-localstorage/register.js
+++
@@ -1,4 +0,0 @@
-if (typeof localStorage === "undefined" || localStorage === null) {
-  const LocalStorage = require('node-localstorage').LocalStorage;
-  global.localStorage = new LocalStorage('./scratch');
-}
