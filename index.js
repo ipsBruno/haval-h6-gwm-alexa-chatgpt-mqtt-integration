577e75c99106eb70dac2ec85a1636c3ea6f5cc80
Bruno da Silva <email@brunodasilva.com>
2024-08-16 05:42:59 -0300

    atualizacao sendcmd

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ async function main() {
     let vin = process.env.VIN;
     let email = process.env.EMAIL
     let senha = process.env.SENHA;
+    let pin = process.env.PIN;
 
-    if(!vin || !email || !senha) {
+    if (!vin || !email || !senha) {
         console.log('Variáveis de ambiente não configuradas');
         return;
     }
@@ -22,20 +23,40 @@ async function main() {
             console.log('Erro ao logar...');
             return;
         }
-    }
 
+    }
+    let vehicles = await carApp.acquireVehicles()
+    console.log('Vehs', vehicles)
     let lastStatus = await carApp.getLastStatus(vin); // ele vai pegar os códigos mqtt atualizados do seu veiculo tem mais outras funcoes em carQuery.js e userQUery.js em desenvolvimento
-    if(!lastStatus) {
+    if (!lastStatus) {
         if (!(await userApp.login(email, senha))) {
             console.log('Erro ao logar...');
             return;
         }
     }
 
-    console.log('Client vehicles:', lastStatus);
+    console.log('Client vehicles:', lastStatus.data.items);
 
     let basicInfo = await carApp.basicInfo();
     console.log('Basic Info logs:', basicInfo);
+    let uniqueId = carApp.uuid()
+    let arCond = await carApp.sendCmd(
+        {
+            "0x04": {
+                "airConditioner": {
+                    "operationTime": "15", // 15 minutos
+                    "switchOrder": "1", // ligado
+                    "temperature": "18" // temperstura 18
+                }
+            }
+        },
+        0,
+        pin,
+        uniqueId,
+        2,
+        vin
+    )
+    console.log(arCond)
 }
 dotenv.config();
 main();
